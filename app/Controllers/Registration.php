2fd6fba38071a3e451153c633364b2983109b5d7
Omar Vega <launian@gmail.com>
2021-03-23 11:02:29 -0600

    Validation Labels

--- app/Controllers/Registration.php
+++ app/Controllers/Registration.php
@@ -10,7 +10,6 @@ class Registration extends Controller
 {
 	public function index()
 	{
-
 		$modelCategories = new CategoriesModel();
 
 		$data['title'] = 'Registrar Usuario';
@@ -20,45 +19,93 @@ class Registration extends Controller
 		echo view('includes/footer');
 	}
 
-	public function register(){
-		$userModel = new UserModel();
-		$modelCategories = new CategoriesModel();
+	public function register()
+	{
 
-		$data['categories'] = $modelCategories->findAll();
+		if ($_POST) {
 
-		helper("url", "form");
+			$userModel = new UserModel();
+			$modelCategories = new CategoriesModel();
+			helper(["url", "form"]);
 
-		$data = [
-			'title' => 'Crear Cuenta',
-			'categories' => $modelCategories->findAll()
-		];
+			$validation = \Config\Services::validation();
+			$data = [
+				'title' => 'Crear Cuenta',
+				'categories' => $modelCategories->findAll()
+			];
 
-		if ($this->request->getMethod() === 'post' && $this->validate([
-			'inputEmail' => 'required',
-			'inputConfirmEmail' => 'required',
-			'inputPassword'  => 'required',
-			'inputName'  => 'required',
-			'inputLastName' => 'required',
-			'inputPhone'  => 'required',
-			'inputCategory' => 'required'
-		])) {
-
-			$userModel->save([
-				'email' => $this->request->getPost('inputEmail'),
-				'password' => $this->request->getPost('inputPassword'),
-				'name' => $this->request->getPost('inputName'),
-				'lastName' => $this->request->getPost('inputLastName'),
-				'phoneNumber' => $this->request->getPost('inputPhone'),
-				'category_id' => $this->request->getPost('inputCategory')
+			$validation->setRules([
+				"inputEmail" => [
+					"label" => "Correo Electrónico",
+					"rules" => "required|valid_email|max_length[100]|is_unique[users.email]",
+					"errors" => [
+						"required" => "El campo {field} es obligatorio. Por favor escribe tu {field}.",
+						"valid_email" => "La dirección de {field} ingresada no es válida.",
+						"max_length" => "El campo de {field} no puede exceder de 100 caracteres."
+					]
+				],
+				"inputConfirmEmail" => [
+					"label" => "Confirmar Correo Electrónico",
+					"rules" => "matches[inputEmail]",
+					"errors" => [
+						"matches" => "Los correos electrónicos no coinciden. Favor de revisarlos."
+					]
+				],
+				"inputPassword" => [
+					"label" => "Contraseña",
+					"rules" => "required|alpha_numeric_punct|min_length[8]|max_length[25]",
+					"errors" => [
+						"required" => "El campo {field} es obligatorio. Por favor elige una contraseña que contenga números, letras y los símbolos !#~$%&*-_+=|:.",
+						"alpha_numeric_punct" => "El campo {field} solo puede contener números, letras y los símbolos !#~$%&*-_+=|:."
+					]
+				],
+				"inputName" => [
+					"label" => "Nombre(s)",
+					"rules" => "required|min_length[3]|max_length[45]",
+					"errors" => [
+						"required" => "El campo Nombre es obligatorio. Por favor escribe tu Nombre.",
+			 			"min_length" => "El campo Nombre debe tener más de tres caracteres."
+					]
+				],
+				"inputLastName" => [
+					"label" => "Apellido(s)",
+					"rules" => "required|min_length[3]|max_length[70]",
+					"errors" => [
+						"required" => "El campo Apellido es obligatorio. Favor de llenarlo.",
+			 			"min_length" => "El campo Apellido debe tener al menos 3 caracteres.",
+			 			"max-length" => "El campo Apellido debe tener menos de 70 caracteres."
+					]
+				],
+				"inputPhone" => [
+					"label" => "Teléfono",
+					"rules" => "required|min_length[10]|max_length[20]|numeric",
+					"errors" => [
+						"required" => "El campo Teléfono es obligatorio. Favor de llenarlo.",
+			 			"min_length" => "El campo Teléfono debe de tener al menos 10 caracteres.",
+			 			"max_length" => "El campo Teléfono debe de tener máximo 20 caracteres.",
+			 			"numeric" => "El campo Teléfono solamente puede contener caracteres numéricos."
+					]
+				],
+				"inputCategory" => [
+					"label" => "Tipo de Usuario",
+					"rules" => "required",
+					"errors" => [
+						"required" => "Favor de escoger cuál es su tipo de usuario.",
+					]
+				]
 			]);
 
-			echo view('includes/header', $data);
-			echo view('RegistrationSuccessView	');
-			echo view('includes/footer');
-		} else {
-			echo view('includes/header', $data);
-			echo view('RegistrationView');
-			echo view('includes/footer');
+
+
+			if (!$validation->withRequest($this->request)->run()) {
+				echo "error<br>";
+				$errors = $validation->getErrors();
+				foreach ($errors as $e) {
+					echo nl2br($e . "\n");
+				}
+			} else {
+				echo "Success";
+			}
 		}
 	}
 }
