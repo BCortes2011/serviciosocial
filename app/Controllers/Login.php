ad9c19f6faa109e9215b83b55f43663aeb33eb5a
Omar Vega <launian@gmail.com>
2021-03-24 17:39:48 -0600

    Implementando sesion de usuario

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -21,18 +21,27 @@ class Login extends BaseController
 
 	public function startSession(){
 		if($_POST){
-			$userModel = new UserModel();
-			$modelCategories = new CategoriesModel();
-			$validation = \Config\Services::validation();
-			$data = [
-				'title' => "Iniciar Sesión",
-				'categories' => $modelCategories->findAll(),
-				'validation' => $validation
-			];
 			
-			echo view('includes/header', $data);
-			echo view('LoginView');
-			echo view('includes/footer');
+			$session = \Config\Services::session();
+			$validation = \Config\Services::validation();
+			
+			$validation->setRules([
+				"inputPassword" => [
+					"label" => "Contraseña",
+					"rules" => "required|alpha_numeric_punct|min_length[8]|max_length[25]|validateUser[inputEmail,inputPassword]",
+					"errors" => [
+						"required" => "El campo {field} es obligatorio. Por favor elige una contraseña que contenga números, letras y los símbolos !#~$%&*-_+=|:.",
+						"alpha_numeric_punct" => "El campo {field} solo puede contener números, letras y los símbolos !#~$%&*-_+=|:.",
+						"validateUser" => "El correo electrónico o la contraseña son incorrectos. Favor de corregirlos"
+					]
+				]
+			]);
+
+			if (!$validation->withRequest($this->request)->run()) {
+				$data['validation'] = $validation;
+			} else {
+				$userModel = new UserModel();
+			}
 		}else{
 			echo "No POST";
 		}
